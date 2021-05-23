19458769627d5dd5ec2e96906f8d5302b026f0d9
Hung Nguyen <nbhung189@gmail.com>
2021-05-23 19:19:42 +0700

    update config theme

--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between py-2 navbar-expand-lg bg-[#333333] text-white tracking-wider">
+      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between py-2 navbar-expand-lg bg-base-base17 text-base-base01 tracking-wider">
         <div className="container mx-auto text-center text-xs font-semibold">
               index.js - hungnb-resume
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const plugin = require("tailwindcss/plugin");
-
 module.exports = {
   mode: "jit",
   purge: {
@@ -118,49 +116,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    plugin(function ({ addComponents, theme }) {
-      const screens = theme("screens", {});
-      addComponents([
-        {
-          ".container": { width: "100%" },
-        },
-        {
-          [`@media (min-width: ${screens.sm})`]: {
-            ".container": {
-              "max-width": "640px",
-            },
-          },
-        },
-        {
-          [`@media (min-width: ${screens.md})`]: {
-            ".container": {
-              "max-width": "768px",
-            },
-          },
-        },
-        {
-          [`@media (min-width: ${screens.lg})`]: {
-            ".container": {
-              "max-width": "1024px",
-            },
-          },
-        },
-        {
-          [`@media (min-width: ${screens.xl})`]: {
-            ".container": {
-              "max-width": "1280px",
-            },
-          },
-        },
-        {
-          [`@media (min-width: ${screens["2xl"]})`]: {
-            ".container": {
-              "max-width": "1280px",
-            },
-          },
-        },
-      ]);
-    }),
-  ],
+  plugins: [],
 };
