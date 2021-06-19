38793a6e72eea13469a729365cf1339a025917b5
Hung Nguyen Ba <nbhung189@gmail.com>
2021-06-19 17:07:00 +0700

    add monaco editor and the tab list

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor from "react-monaco-editor";
 
-const Editor = () => {
+const Editor = ({ content, language }) => {
+  const opts = {
+    readOnly: true
+  }
 	return (
+    <div className="flex flex-col w-full">
+    <div>abcd</div>
     <MonacoEditor
-     height="90vh"
-     defaultLanguage="javascript"
+     language="javascript"
+     theme="vs-dark"
+     options={opts}
      defaultValue="// some comment"
    />
+   </div>
   )
 }
 
--- src/containers/Index.js
+++ src/containers/Index.js
@@ -4,15 +4,16 @@ import React from "react";
 import StatusBar from "components/StatusBar";
 import ActivityBar from "components/ActivityBar";
 import Explorer from "components/Explorer";
+import Editor from "components/Editor";
 
 export default function Index() {
   return (
     <>
       <div className="container subpixel-antialiased">
-        <div className="absolute left-0 flex h-full">
+        <div className="absolute left-0 flex h-full w-full pb-6">
           <ActivityBar />
           <Explorer />
-          <div className="bg-accents-red02"></div>
+          <Editor content="" />
         </div>
         <StatusBar />
       </div>
